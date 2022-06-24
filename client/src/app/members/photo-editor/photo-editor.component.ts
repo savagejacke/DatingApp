e5efb3c996e83f26f1977a5edb4f95ecebeff039
Jack Savage <jsavage@costar.com>
2022-06-24 13:25:23 -0400

    End of Section 12

--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -73,8 +73,13 @@ export class PhotoEditorComponent implements OnInit {
     }
     this.uploader.onSuccessItem = (item, response, status, headers) => {
       if (response) {
-        const photo = JSON.parse(response);
+        const photo: Photo = JSON.parse(response);
         this.member.photos.push(photo);
+        if (photo.mainPhoto) {
+          this.user.photoUrl = photo.url;
+          this.member.photoUrl = photo.url;
+          this.accoutnService.setCurrentUser(this.user);
+        }
       }
     }
   }
